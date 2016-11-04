b34be6e0af55d093233623675761d3ac96909a3f
Dominik Niedermaier <galaxycrysis11@gmail.com>
2016-11-04 20:49:37 +0100

    updated kanji reading

--- src/mainWindow.py
+++ src/mainWindow.py
@@ -4,8 +4,17 @@ class Menu(QtGui.QMainWindow):
     def __init__(self):
         super(Menu,self).__init__()
         uic.loadUi("../ui/main.ui",self)
-        self.readButton.clicked.connect(self.readkanji)
+        self.readButton.clicked.connect(self.startReading)
         self.show()
+
+
+##################Reading Kanjis Functions############################
+    #open the gui for reading kanjis and init it
+    def startReading(self):
+        uic.loadUi("../ui/read_vocab.ui",self)
+
+
+
 
 
 
